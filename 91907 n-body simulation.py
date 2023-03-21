d31df90526dd07538071dd0079ef07c3267b6ed1
Regan <reganclare11@gmail.com>
2023-03-21 13:17:34 +1300

    Creating Canvas

--- 91907 n-body simulation.py
+++ 91907 n-body simulation.py
@@ -1,2 +1,11 @@
-import matplotlib.pyplot
-import math
+import numpy as np  # this library is used for linear algebra. I.e. matrix algebra
+import matplotlib.pyplot as plt  # use for plotting data on a canvas
+import math  # allows the use of mathematical constants
+import matplotlib.animation as animation  # allows for animating the particles
+import tkinter
+
+#CANVAS
+fig = plt.figure(figsize=(10, 10))
+ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
+
+plt.show()
