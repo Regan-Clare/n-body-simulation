e3aaf6ac3e55a22988ded1c55c708fd39c43f1e4
Regan Clare <reganclare11@gmail.com>
2023-03-29 15:44:48 +1300

    Animating black particles

--- 91907 n-body simulation.py
+++ 91907 n-body simulation.py
@@ -7,15 +7,18 @@ fig = plt.figure(figsize=(10, 10))  # the size of the canvas
 ax = plt.axes(xlim=(0, 10), ylim=(0, 10))  # the size of the axis points
 
 # defining initial conditions
-EM = 5  # 5.972e24 Earth's Mass
-G = 6.6743e-11  # Gravitational constant
-dt = 0.1  # this is the time step. dt meaning change in time.
 NumParticles = int(input("How many particles in this system?: "))
+m = 2  # 5.972e24 Earth's Mass
+dt = 0.1  # this is the time step. dt meaning change in time.
 
-m = [EM] * NumParticles
-P = np.random.rand(NumParticles, 2) * 9.8  # initial positions [x, y], at 9.8 so, it doesn't hit the edge
-v = np.zeros((NumParticles, 2))  # initial velocities [dx, dy]
-dP = []  # new positions [cy, cx]
+
+x = np.random.uniform(low=0.2, high=9.8, size=NumParticles) # initial positions [x]
+y = np.random.uniform(low=0.2, high=9.8, size=NumParticles)  # initial positions [y]
+vx = np.random.uniform(low=0, high=0.1, size=NumParticles)  # initial x-velocity
+vy = np.random.uniform(low=0, high=0.1, size=NumParticles)  # initial y-velocity
+
+cx = [[] for _ in range(NumParticles)]  # new positions for x-axis
+cy = [[] for _ in range(NumParticles)]  # new positions for y-axis
 
 curve, = ax.plot([], [])
 obj, = plt.plot([], [])
@@ -24,36 +27,40 @@ obj, = plt.plot([], [])
 def initial_conditions():
     curve.set_data([], [])
     obj.set_data([], [])
+    # here, scatter is a method to plot objects and uses ax as a reference frame
+    # this means that when 'i' is the number of particles, then...
+    # the scatter method plots the position, size (s), and colour of the particles on the canvas
+    # the [i, 0] and [i, 1] extract the x and y coordinates of the i-th particle respectively.
+    # 0 being the first column x, and 1 being the second column y
+    # these basically refer back to the matrices/arrays I made earlier
+    for i in range(NumParticles):
+        ax.scatter(x, y, s=40, c='black')  # x-position, y-position, size, colour
     return curve, obj,
 
 
 initial_conditions()
 
-# here, scatter is a method to plot objects and uses ax as a reference frame
-# this means that when 'i' is the number of particles, then...
-# the scatter method plots the position, size (s), and colour of the particles on the canvas
-# the [i, 0] and [i, 1] extract the x and y coordinates of the i-th particle respectively.
-# 0 being the first column x, and 1 being the second column y
-# these basically refer back to the matrices/arrays I made earlier
-for i in range(NumParticles):
-    ax.scatter(P[i, 0], P[i, 1], s=40, c='black')  # x-position, y-position, size, colour
-
 
 def update(frame):
-    global P, v, r
-    for i in range(2):
-        j = (i + 1) % 2
-        v[i, 0] += dt * m[j] * (P[j, 0] - P[i, 0]) / ((P[j, 0] - P[i, 0]) ** 2 + (P[j, 1] - P[i, 1]) ** 2) ** 1.5
-        v[i, 1] += dt * m[j] * (P[j, 1] - P[i, 1]) / ((P[j, 0] - P[i, 0]) ** 2 + (P[j, 1] - P[i, 1]) ** 2) ** 1.5
-        v[2, 0] += dt * m[i] * (P[i, 0] - P[2, 0]) / ((P[i, 0] - P[2, 0]) ** 2 + (P[i, 1] - P[2, 1]) ** 2) ** 1.5
-        v[2, 1] += dt * m[i] * (P[i, 1] - P[2, 1]) / ((P[i, 0] - P[2, 0]) ** 2 + (P[i, 1] - P[2, 1]) ** 2) ** 1.5
-    for i in range(3):
-        P[i, 0] += dt * v[i, 0]
-        P[i, 1] += dt * v[i, 1]
-        dP[i].append(P[i, 0])
-        dP[i].append(P[i, 1])
-    curve.set_data(dP[i])
-    obj.set_data(P[i, 0], P[i, 1])
+    global x, y, vx, vy, r
+    for i in range(NumParticles):
+        for j in range(i + 1, NumParticles):
+            dx = x[j] - x[i]
+            dy = y[j] - y[i]
+            r = np.sqrt(dx ** 2 + dy ** 2)
+            fx = m * dx / (r ** 3)
+            fy = m * dy / (r ** 3)
+            vx[i] += dt * fx
+            vy[i] += dt * fy
+            vx[j] -= dt * fx
+            vy[j] -= dt * fy
+        x[i] += dt * vx[i]
+        y[i] += dt * vy[i]
+        cx[i].append(x[i])
+        cy[i].append(y[i])
+    curve.set_data(cx, cy)
+    obj.set_data(x, y)
+
     return curve, obj,
 
 
