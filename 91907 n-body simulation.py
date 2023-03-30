cda8789d9e1eec0ec0feb5325341021f526a7db5
Regan <reganclare11@gmail.com>
2023-03-30 13:17:52 +1300

    MOVING PARTICLES - now trying to fix lines

--- 91907 n-body simulation.py
+++ 91907 n-body simulation.py
@@ -9,10 +9,10 @@ ax = plt.axes(xlim=(0, 10), ylim=(0, 10))  # the size of the axis points
 # defining initial conditions
 NumParticles = int(input("How many particles in this system?: "))
 m = 2  # 5.972e24 Earth's Mass
-dt = 0.1  # this is the time step. dt meaning change in time.
+dt = 0.01  # this is the time step. dt meaning change in time.
 
 
-x = np.random.uniform(low=0.2, high=9.8, size=NumParticles) # initial positions [x]
+x = np.random.uniform(low=0.2, high=9.8, size=NumParticles)  # initial positions [x]
 y = np.random.uniform(low=0.2, high=9.8, size=NumParticles)  # initial positions [y]
 vx = np.random.uniform(low=0, high=0.1, size=NumParticles)  # initial x-velocity
 vy = np.random.uniform(low=0, high=0.1, size=NumParticles)  # initial y-velocity
@@ -20,29 +20,35 @@ vy = np.random.uniform(low=0, high=0.1, size=NumParticles)  # initial y-velocity
 cx = [[] for _ in range(NumParticles)]  # new positions for x-axis
 cy = [[] for _ in range(NumParticles)]  # new positions for y-axis
 
+scatter_list = []
+for i in range(NumParticles):
+    cx[i] = []
+    cy[i] = []
+    scatter, = ax.plot([], [], 'o', markersize=5, color='black')
+    scatter_list.append(scatter)
+
+line, = ax.plot([], [], '-', color='blue')
+
 curve, = ax.plot([], [])
-obj, = plt.plot([], [])
 
 
 def initial_conditions():
-    curve.set_data([], [])
-    obj.set_data([], [])
-    # here, scatter is a method to plot objects and uses ax as a reference frame
-    # this means that when 'i' is the number of particles, then...
-    # the scatter method plots the position, size (s), and colour of the particles on the canvas
-    # the [i, 0] and [i, 1] extract the x and y coordinates of the i-th particle respectively.
-    # 0 being the first column x, and 1 being the second column y
-    # these basically refer back to the matrices/arrays I made earlier
     for i in range(NumParticles):
-        ax.scatter(x, y, s=40, c='black')  # x-position, y-position, size, colour
-    return curve, obj,
+        scatter_list[i].set_data(x[i], y[i])
+
+    line.set_data([], [])
+
+    return scatter_list + [line]
 
 
 initial_conditions()
 
+r = 0
+
 
 def update(frame):
     global x, y, vx, vy, r
+
     for i in range(NumParticles):
         for j in range(i + 1, NumParticles):
             dx = x[j] - x[i]
@@ -50,18 +56,17 @@ def update(frame):
             r = np.sqrt(dx ** 2 + dy ** 2)
             fx = m * dx / (r ** 3)
             fy = m * dy / (r ** 3)
-            x[i] += dt * fx
-            y[i] += dt * fy
-            x[j] -= dt * fx
-            y[j] -= dt * fy
+            vx[i] += dt * fx
+            vy[i] += dt * fy
+            vx[j] -= dt * fx
+            vy[j] -= dt * fy
         x[i] += dt * vx[i]
         y[i] += dt * vy[i]
-        cx[i].append(x[i])
-        cy[i].append(y[i])
-    curve.set_data(cx, cy)
-    obj.set_data(x, y)
+        scatter_list[i].set_data(x[i], y[i])
 
-    return curve, obj,
+    line.set_data(x, y)
+
+    return scatter_list + [line]
 
 
 ani = anim.FuncAnimation(fig, update, init_func=initial_conditions, interval=1, blit=True, save_count=9000)
